051a961113f5f52bb733fdcc645748c432d76bdd
Daniel <dbart@danielvbart.com>
2019-12-02 19:31:38 +0000

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/*
+ *
+ * @author Daniel Bart
+ */
+package com.dvb.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Daniel!");
+    }
+}
